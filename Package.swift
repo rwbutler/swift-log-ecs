28a85631204253a2e43fa9eb795e1346afcd6000
Ross Butler <github@rwbutler.com>
2024-05-11 17:29:00 +0100

    Added platforms to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "swift-log-ecs",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "ECSLogging",
